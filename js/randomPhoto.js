f000962cdf10cd6721722e7b356958a6c14b6abd
Tyler Hill <Tyler@glarmsi.local>
2020-11-18 15:49:10 -0500

    added photos

--- js/randomPhoto.js
+++
@@ -1,7 +0,0 @@
-var images = ["me.jpg","nikon-d750-review-body-angle.jpg"];
-
-function photo()
-
-	var rand = Math.floor(Math.random() * images.length);
-	
-	document.getElementById("mypicture").src = images[rand];
